191a4a82493641330b5ce663c5e85fdf7225521c
peterjinchristiansen <82626937+peterjinchristiansen@users.noreply.github.com>
2021-09-11 19:46:37 -0600

    API is now functional

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var baseUrl = 'https://api.openweathermap.org/data/2.5/onecall';
-var apiKey = '&appid=db0a794ac59eac6a53f36f784e33fb8a';
+var searchBarEl = document.getElementById('searchBar');
+var cityFormEl = document.getElementById('cityForm');
+var cityDisplayEl = document.getElementById('cityDisplay');
 
-fetch(baseUrl + apiKey).then(function(response) {
-	return response.json();
-}).then(function(response){
-	console.log(response)
-});
+
+cityFormEl.onsubmit = submit;
+
+function submit(event) {
+	event.preventDefault();
+	var api_2 = '&appid=35da9ea72100aa99f12e733919325eb0';
+	var api_1 = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+	var cityName = searchBarEl.value.trim();
+	apiurl = (api_1 + cityName + api_2);
+
+	return fetch(apiurl)
+		.then(function(response) {
+			return response.json();})
+		.then(function(data) {
+			console.log(data);
+			var city = (data.city.name)
+			cityDisplayEl.innerHTML = city;	
+		})
+}
